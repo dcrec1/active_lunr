7f6fc3d5fc9fc91afd74a3eebd218c572f23a743
Diego Carrion <dc.rec1@gmail.com>
2009-11-29 22:15:21 -0200

    a model should be posted to /documents

--- lib/activelunr.rb
+++ lib/activelunr.rb
@@ -3,6 +3,6 @@ module ActiveLunr
   URL = YAML::load_file("#{RAILS_ROOT}/config/lunr.yml")[RAILS_ENV]
   
   def after_create
-    RestClient.post(URL, :document => @attributes.merge('_type' => self.class.to_s))
+    RestClient.post("#{URL}/documents", :document => @attributes.merge('_type' => self.class.to_s))
   end
 end
--- spec/activelunr_spec.rb
+++ spec/activelunr_spec.rb
@@ -4,11 +4,32 @@ class Advertise < ActiveRecord::Base
   include ActiveLunr
 end
 
+def stub_post
+  RestClient.stub!(:post)
+end
+
+def expect_post_with(*params)
+  RestClient.should_receive(:post).with(*params)
+end
+
+def expect_put_with(*params)
+  RestClient.should_receive(:put).with(params)
+end
+
 describe ActiveLunr do
-  context "on creation" do
+  context "on create" do
     it "should post a Lunr document with the attributes, the type and the id" do
-      RestClient.should_receive(:post).with("http://host:1234/context", :document => {'name' => "Tito", 'lastname' => "Ortiz", '_type' => 'Advertise', 'id' => 1})
+      expect_post_with("http://host:1234/context/documents", :document => {'name' => "Tito", 'lastname' => "Ortiz", '_type' => 'Advertise', 'id' => 1})
       Advertise.create! :name => "Tito", :lastname => "Ortiz"
+    end
+  end
+  
+  context "on update" do
+    it "should put a Lunr document with the attributes" do
+      stub_post
+      advertise = Advertise.create! :name => "Vanderlei"
+      expect_put_with("http://host:1234/context/documents/#{advertise.id}", :document => {'city' => "São Paulo"})
+      advertise.update_attributes! :city => "Lyoto"
     end
   end
 end
