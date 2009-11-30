0af627b284db7c06e88db75e2c78c9b30f853813
Diego Carrion <dc.rec1@gmail.com>
2009-11-29 22:28:24 -0200

    refactors

--- lib/activelunr.rb
+++ lib/activelunr.rb
@@ -1,12 +1,12 @@
 module ActiveLunr
   
-  URL = YAML::load_file("#{RAILS_ROOT}/config/lunr.yml")[RAILS_ENV]
+  DOCUMENTS_URL = YAML::load_file("#{RAILS_ROOT}/config/lunr.yml")[RAILS_ENV] + '/documents'
   
   def after_create
-    RestClient.post("#{URL}/documents", :document => @attributes.merge('_type' => self.class.to_s))
+    RestClient.post(DOCUMENTS_URL, :document => @attributes.merge('_type' => self.class.to_s))
   end
   
   def after_update
-    RestClient.put("#{URL}/documents/#{id}", :document => @attributes.delete_if {|k, v| v.nil? or k.eql?('id')})
+    RestClient.put("#{DOCUMENTS_URL}/#{id}", :document => @attributes.delete_if {|key, value| value.nil? or key.eql?('id')})
   end
 end
