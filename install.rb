3294bb6130394ce195110c446005ea5ece7aaa57
Diego Carrion <dc.rec1@gmail.com>
2009-11-30 01:32:43 -0200

    lunr.yml is copied on install

--- install.rb
+++ install.rb
@@ -1,1 +1,6 @@
-# Install hook code here
+require 'fileutils'
+ 
+src = File.join(File.dirname(__FILE__), 'lunr.yml')
+target = File.join(File.dirname(__FILE__), '..', '..', '..', 'config', 'lunr.yml')
+
+FileUtils.cp src, target
